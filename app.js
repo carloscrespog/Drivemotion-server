038a6509c155cff887b29a581b66f6bf0ca2ab31
carloscrespog <carlos.crespog@gmail.com>
2013-05-07 07:21:40 +0200

    VErsion inestable

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var server=http.createServer(app).listen(app.get('port'), function(){
 var io = require('socket.io').listen(server);
 io.configure(function () {
   io.set("transports", ["xhr-polling"]);
-  io.set("polling duration", 1000);
+  io.set("polling duration", 10);
 });
 io.sockets.on('connection', function(socket){
   clientConnect(socket);
